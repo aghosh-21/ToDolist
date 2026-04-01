89b37e79c07141c47a0cc52aac381fa1174355ff
aghosh-21 <ananya04ghosh@gmail.com>
2026-04-01 12:52:32 +0530

    2nd time trying to understand usestate

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
+import "./ToDoList.css";
+
 export default function ToDoList(){
+    let [todos,setTodos]=useState(["simple Task"]);
+    let [newTodo,setNewTodo]=useState("");
+
+    let addNewTask=()=>{
+        console.log("we have to add new task in todo");
+    }
     return(
-        <div>
-            <input type="text" placeholder="add your task" />
+        <div className="todo">
+            <input type="text" placeholder="add your task" value={newTodo} />
             <button>add task</button>
             <br /><br /><br /><br />
             <hr />
             <h4>ToDo List</h4>
-            <ul></ul>
+            <ul>
+                {
+                    todos.map((todo)=>{
+                         <li>{todo}</li>;
+                    })
+                }
+            </ul>
         </div>
     )
 }
