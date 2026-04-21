9643a0176ecc22a19e82bc47539baad4c55d173e
aghosh-21 <ananya04ghosh@gmail.com>
2026-04-22 01:27:22 +0530

    ok

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -3,20 +3,56 @@ import "./ToDoList.css";
 import { v4 as uuidv4 } from "uuid";
 
 export default function ToDoList() {
-  let [todos, setTodos] = useState([{ task: "simple task", id: uuidv4() }]);
+  let [todos, setTodos] = useState([
+    { task: "simple task", id: uuidv4(), done: false },
+  ]);
   let [newTodo, setNewTodo] = useState("");
 
- let addNewTask = () => {
-  setTodos((prevTodo) => [
-    ...prevTodo, { task: newTodo, id: uuidv4() }
-  ]);
-  setNewTodo("");
-};
+  let addNewTask = () => {
+    if (newTodo != "")
+      setTodos((prevTodo) => [
+        ...prevTodo,
+        { task: newTodo, id: uuidv4(), done: false },
+      ]);
+    setNewTodo("");
+  };
 
- 
   let getTask = (e) => {
     //console.log(e.target.value);
     setNewTodo(e.target.value);
+  };
+
+  let dltButton = (key) => {
+    let copy = todos.filter((todo) => todo.id != key);
+    //console.log(copy);
+    setTodos(copy);
+  };
+
+  let updtMany = () => {
+    let uptCopy = todos.map((todo) => {
+      return {
+        ...todo,
+        task: todo.task.toUpperCase(),
+      };
+    });
+    //console.log(uptCopy);
+    // setTodos([uptCopy]);//it became nested copy beacaue uptCopy already an array
+    setTodos(uptCopy);
+  };
+
+  let markDownBtn = (key) => {
+    console.log(key);
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id == key) {
+          return {
+            ...todo,
+            done: !todo.done,
+          };
+        }
+        else return todo
+      }),
+    );
   };
   return (
     <div className="todo">
@@ -36,11 +72,26 @@ export default function ToDoList() {
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
-           <span>{todo.task}</span> 
-           <span><button>delete</button></span> 
-            </li> // write key=... not id or other name
+            {todo.done == true ? (
+              <i className="fa-solid fa-square-check"></i>
+            ) : (
+              <i className="fa-regular fa-square"></i>
+            )}
+            &nbsp; &nbsp;
+            <span>{todo.task}</span>
+            &nbsp; &nbsp;
+            <span>
+              <button onClick={() => dltButton(todo.id)}>delete</button>
+              {/* arrow function doesn't execute the program just make a copy */}
+              &nbsp; &nbsp;
+              <button onClick={() => markDownBtn(todo.id)}>Mark/Unmark</button>
+            </span>
+          </li> // write key=... not id or other name
         ))}
       </ul>
+      <hr />
+      <hr />
+      <button onClick={updtMany}>update ALL</button>
     </div>
   );
 }
